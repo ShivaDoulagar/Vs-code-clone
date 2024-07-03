08383ad91812fe8bba97705fb0b2b144acb31221
ShivaDoulagar <149285341+ShivaDoulagar@users.noreply.github.com>
2024-07-03 23:00:40 +0530

    Initial commit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,7 +7,7 @@ const nav = () => {
     <nav>
       <div className='logo' >
         <img className='image' src="https://imgs.search.brave.com/1tkVRf1uC7LGdLaLOasZmLnvd22f2M1B7akfXXyVde0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9sb2dv/dHlwLnVzL2ZpbGUv/dnMtY29kZS5zdmc" alt="" />
-        <h2><a className='home'style={{color:"white",textDecoration:"none"}} href="https:/google.com">Visual Studio Code</a></h2>
+        <h2><a className='home'style={{color:"white",textDecoration:"none"}} href="https://shivadoulagar.github.io/Vs-code-clone/">Visual Studio Code</a></h2>
         <ol>
           <li><a href="https://code.visualstudio.com/docs">Docs</a></li>
           <li><a href="https://code.visualstudio.com/updates/v1_90">Updates</a></li>
